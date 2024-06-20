d6467bfef4fa24ac4f7f6949b83e85d1e3c45956
gavin lou <gavinlou@kingsphase.page>
2024-06-20 13:01:22 +0800

    json read param implement

---
+++ test.py
@@ -0,0 +1,8 @@
+import headVOC_package
+from headVOC_package import c2v_headVOC, v2c_headVOC
+
+# Create a c2v_headVOC object
+c2v = c2v_headVOC()
+# Create a v2c_headVOC object
+v2c = v2c_headVOC()
+
